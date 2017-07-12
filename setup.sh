fe97522b7cddd1e72592109d68786d5dbb3949a7
Jesse McFarland <jesse.m.mcfarland3@gmail.com>
2017-07-11 20:27:48 -0400

    Check-in setup script
    * Backup existing files and directories
    * Link files and directories to repo

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e -u
+
+BACKUP="${HOME?}/.dotfiles_backup"
+DOTFILES=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PACKAGES=(editorconfig git ruby tmux vim zsh)
+
+mkdir -p "${BACKUP?}"
+for PKG in "${PACKAGES[@]?}"; do
+    find "${DOTFILES?}/${PKG?}" -mindepth 1 -maxdepth 1 |\
+        while read CONFIG; do
+            CONFIG_LINK="${HOME?}/$(basename ${CONFIG?})"
+            if [[ ! -L "${CONFIG_LINK?}" ]]; then
+                mv "${CONFIG_LINK?}" "${BACKUP?}"
+            fi
+            ln -frsT "${CONFIG?}" "${CONFIG_LINK?}"
+        done
+done
