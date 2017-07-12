0c5fb39ef5e175abc207b46c34d6305830789bb6
Jesse McFarland <jesse.m.mcfarland3@gmail.com>
2017-07-11 20:33:22 -0400

    Add git submodule update to setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ BACKUP="${HOME?}/.dotfiles_backup"
 DOTFILES=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 PACKAGES=(editorconfig git ruby tmux vim zsh)
 
+git submodule update --init --merge --remote
 mkdir -p "${BACKUP?}"
 for PKG in "${PACKAGES[@]?}"; do
     find "${DOTFILES?}/${PKG?}" -mindepth 1 -maxdepth 1 |\
