e0b186a9a0abf8d48826f38b280e0c4aedd07eed
Jesse McFarland <jesse@mcfarland.sh>
2017-07-26 07:08:56 -0400

    Fix for rbenv ruby install

--- setup.sh
+++ setup.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 set -e -u
 
-BACKUP="${HOME?}/.dotfiles_backup"
+BACKUP="${HOME}/.dotfiles_backup"
 DOTFILES=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 PACKAGES=(editorconfig git ruby tmux vim zsh)
+RUBY_BUILD="${HOME}/.rbenv/plugins/ruby-build"
 RUBY_VERSION='2.4.1'
 
 git submodule update --init --merge --remote
-mkdir -p "${BACKUP?}"
-for PKG in "${PACKAGES[@]?}"; do
-    find "${DOTFILES?}/${PKG?}" -mindepth 1 -maxdepth 1 |\
+mkdir -p "${BACKUP}"
+for PKG in "${PACKAGES[@]}"; do
+    find "${DOTFILES}/${PKG}" -mindepth 1 -maxdepth 1 |\
         while read CONFIG; do
-            CONFIG_LINK="${HOME?}/$(basename ${CONFIG?})"
-            if [[ ! -L "${CONFIG_LINK?}" && -e "${CONFIG_LINK?}" ]]; then
-                mv "${CONFIG_LINK?}" "${BACKUP?}"
+            CONFIG_LINK="${HOME}/$(basename ${CONFIG})"
+            if [[ ! -L "${CONFIG_LINK}" && -e "${CONFIG_LINK}" ]]; then
+                mv "${CONFIG_LINK}" "${BACKUP}"
             fi
-            ln -frsT "${CONFIG?}" "${CONFIG_LINK?}"
+            ln -frsT "${CONFIG}" "${CONFIG_LINK}"
         done
 done
+
 cd ${HOME}/.rbenv && src/configure && make -C src
-RBENV_INIT=$(rbenv init 2>&1 | sed -e '/^#/d' -e '/^$/d')
-${RBENV_INIT} && rbenv install ${RUBY_VERSION?}
+mkdir -p ${RUBY_BUILD}
+[[ -d ${RUBY_BUILD}/.git ]] ||\
+    git clone https://github.com/rbenv/ruby-build.git ${RUBY_BUILD}
+rbenv install -s ${RUBY_VERSION}
